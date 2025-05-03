fba57c0e0507578d1da95c48ebd0db93c86d0259
rush <rush@gmail.com>
2025-05-03 14:07:10 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'home/index.html')
+
+def about(request):
+   return render(request, 'home/about.html')
+
+def booking(request):
+    return render(request, 'home/booking.html')
+
+def doctors(request):
+   return render(request, 'home/doctors.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def department(request):
+    return render(request, 'home/department.html')
